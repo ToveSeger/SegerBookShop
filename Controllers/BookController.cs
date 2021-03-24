475efdbb3b75e5f41d0058161fd74b79357cbe5a
Tove Seger <tove.seger@outlook.com>
2021-03-24 08:52:07 +0100

    bigpush

---
+++ Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace SegerBookShop.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult ShowSearchResults(int id)
+        //{
+        //    BookShop.WebbShopAPI api = new BookShop.WebbShopAPI();
+        //    var searchResult = api.GetBook(id).FirstOrDefault();
+        //    return View();
+        //}
+
+        public async Task<IActionResult> ShowSearchResults(int id)
+        {
+            BookShop.WebbShopAPI api = new BookShop.WebbShopAPI();
+            var searchResult = api.GetBook(id).ToList();          
+            return View(searchResult);
+        }
+
+        public async Task<IActionResult> ShowSearchResultsWithKeyword(string keyword)
+        {
+            BookShop.WebbShopAPI api = new BookShop.WebbShopAPI();
+            var searchResult = api.GetBooks(keyword).ToList();
+            return View(searchResult);
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegerBookShop.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> LoginVerification(string userName, string passWord) //Fortsätt här, fel vy returneras
+        {
+            BookShop.WebbShopAPI api = new BookShop.WebbShopAPI();
+            var login = api.Login(userName, passWord);
+            if (login != 0)
+            {
+                return View("LoginVerification", login);
+            }
+           
+                return View("LoginFailed");
+           
+
+        }
+    }
+}
